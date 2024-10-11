5cdc71c25d3560cd8151d0d3a6970644dcf9e9b7
Brayan Durán <brayan13s133@gmail.com>
2024-10-11 16:40:57 -0400

    env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,11 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+require('dotenv').config();
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
